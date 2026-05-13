001efd3dd2578498b410018c2a6c3c9a311cd34b
tetsu74 <hirataira21@gmail.com>
2026-05-13 13:48:54 +0900

    Update Service Worker to force activation of new versions

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,14 @@
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+  console.log('Service Worker: Activated');
 });
 
 self.addEventListener('fetch', (event) => {
   // Simple pass-through for now. 
-  // You can add caching logic here for offline support later if needed.
   event.respondWith(fetch(event.request));
 });
